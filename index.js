65be50ff77b0b5d076bcf7b9a11d22919e4d55c2
Nathan <chung.nathanh@gmail.com>
2020-08-24 14:57:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // Clearode your solutions in this file
-const writeCards()
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
